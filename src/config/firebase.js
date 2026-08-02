a6e8815234ba404ae79ae3c1d920983d8e23c4ad
tdat <datnguyen4804@gmail.com>
2026-08-03 02:43:09 +0700

    Update firebase config for vido-student-beta

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,25 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
 
 if (!admin.apps.length) {
   let serviceAccount;
-  if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+  const jsonPath = path.join(__dirname, '../../vido-student-beta-firebase-adminsdk-4rkqd-b41028adde.json');
+
+  if (fs.existsSync(jsonPath)) {
+    serviceAccount = require(jsonPath);
+  } else if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
     serviceAccount = JSON.parse(
       Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8')
     );
-  } else {
+  } else if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   }
+
+  if (serviceAccount && serviceAccount.private_key) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  }
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
